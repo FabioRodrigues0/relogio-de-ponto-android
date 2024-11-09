659bbb294e511c16238ee7b9bfb345a02225cd31
Fábio Rodrigues <56126547+FabioRodrigues0@users.noreply.github.com>
2024-11-09 00:53:45 +0000

    Alteração do gradle para com a adição de algumas librarias nessessarias

--- MobCesaePulse/app/build.gradle.kts
+++ MobCesaePulse/app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
 	alias(libs.plugins.android.application)
-	alias(libs.plugins.kotlin.android)
-	alias(libs.plugins.kotlin.compose)
+	alias(libs.plugins.jetbrains.kotlin.android)
+	alias(libs.plugins.kotlin.kapt)
+	alias(libs.plugins.hilt.android)
+	alias(libs.plugins.compose.compiler)
+	alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
 	namespace = "com.cesaepulse.app"
-	compileSdk = 34
+	compileSdk = 35
 
 	defaultConfig {
 		applicationId = "com.cesaepulse.app"
-		minSdk = 27
+		minSdk = 24
 		targetSdk = 34
 		versionCode = 1
 		versionName = "1.0"
@@ -34,24 +37,73 @@ android {
 	buildFeatures {
 		compose = true
 	}
+	composeOptions {
+		kotlinCompilerExtensionVersion = "1.5.14"
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
+}
+
+kapt {
+	correctErrorTypes = true
 }
 
 dependencies {
 
-	implementation(libs.androidx.material.icons.extended)
+	implementation(libs.coil.compose)
+	implementation(libs.material)
+
 	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.lifecycle.runtime.ktx)
+
+	implementation(libs.androidx.activity.ktx)
 	implementation(libs.androidx.activity.compose)
+
+	// MARK: - Lifecycle
+	implementation(libs.androidx.lifecycle.viewmodel.ktx)
+	implementation(libs.androidx.lifecycle.viewmodel.compose)
+	implementation(libs.androidx.lifecycle.runtime.compose)
+	implementation(libs.androidx.lifecycle.runtime.ktx)
+	implementation(libs.androidx.lifecycle.livedata.ktx)
+
+	implementation(libs.androidx.work.runtime.ktx)
+
+	// MARK: - Navigation
+	implementation(libs.androidx.navigation.ui.ktx)
+	implementation(libs.androidx.navigation.compose)
+
+	// MARK: - Moshi
+	implementation(libs.squareup.retrofit)
+	implementation(libs.retrofit2.converter.moshi)
+	implementation(libs.squareup.moshi.kotlin)
+	implementation(libs.skydoves.sandwich.retrofit)
+
+	implementation(libs.androidx.ui.text.google.fonts)
 	implementation(platform(libs.androidx.compose.bom))
-	implementation(libs.androidx.ui)
-	implementation(libs.androidx.ui.graphics)
-	implementation(libs.androidx.ui.tooling.preview)
-	implementation(libs.androidx.material3)
-	testImplementation(libs.junit)
-	androidTestImplementation(libs.androidx.junit)
-	androidTestImplementation(libs.androidx.espresso.core)
-	androidTestImplementation(platform(libs.androidx.compose.bom))
-	androidTestImplementation(libs.androidx.ui.test.junit4)
-	debugImplementation(libs.androidx.ui.tooling)
-	debugImplementation(libs.androidx.ui.test.manifest)
+	implementation(libs.androidx.compose.ui)
+	implementation(libs.androidx.compose.graphics)
+	implementation(libs.androidx.compose.ui.tooling.preview)
+	implementation(libs.androidx.compose.material3)
+	implementation(libs.androidx.material.icons.extended)
+
+	implementation(libs.androidx.navigation.compose)
+	implementation(libs.kotlinx.serialization.json)
+
+	// MARK: - HTTP Client + Utilities
+	implementation(libs.squareup.retrofit)
+	implementation(libs.converter.scalars)
+	implementation(platform(libs.okhttp.bom))
+	implementation(libs.okhttp)
+	implementation(libs.okhttp.logging.interceptor)
+
+	// MARK: - Hilt (dagger/hilt)
+	implementation(libs.hilt.android)
+	kapt(libs.hilt.compiler)
+	kapt(libs.hilt.android.compiler)
+	implementation(libs.androidx.hilt.navigation.compose)
+	kapt(libs.androidx.hilt.compiler)
 }
+
+// Allow references to generated code
