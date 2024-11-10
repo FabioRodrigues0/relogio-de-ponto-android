d0272c4f64d115cf93b9e353c9dff418ab7d0b1e
joana <joanarosinhan@gmail.com>
2024-11-10 02:45:41 +0000

    sds
    sdsd

--- WebCesaePulse/resources/views/master/master.blade.php
+++ WebCesaePulse/resources/views/master/master.blade.php
@@ -14,7 +14,7 @@
     </script>
     <script src="{{ asset('JS/app.js') }}" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Boxicons -->
     <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
     <scriptsrc="https://kit.fontawesome.com/d5fba335cd.js" crossorigin="anonymous"></scriptsrc=>
@@ -30,10 +30,10 @@
         <header class="header" id="header">
 
             <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-      <div>
+            <div>
 
 
-            <div class="header_img"> <img src="{{asset('images/rapaz.png')}}" alt="logocesae"> </div>
+                <div class="header_img"> <img src="{{ asset('images/rapaz.png') }}" alt="logocesae"> </div>
 
         </header>
 
@@ -47,7 +47,8 @@
                                 class='bx bx-user nav_icon'></i>
                             <span class="nav_name">Meu Perfil</span> </a> <a href="#" class="nav_link"> <i
                                 class='bx bx-message-square-detail nav_icon'></i>
-                            <span class="nav_name">Notificações e Lembretes </span> </a> <a href="#" class="nav_link">
+                            <span class="nav_name">Notificações e Lembretes </span> </a> <a href="#"
+                            class="nav_link">
                             <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Cesae Comunidade</span> </a>
                         <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i>
                             <span class="nav_name">Histórico</span> </a> <a href="#" class="nav_link"> <i
@@ -61,12 +62,18 @@
         </div>
 
         <!--Container Main start-->
-        <main class="height-100 bg-light">
-            <div class="banner_img"> <img src="{{asset('images/cesaeimg.png')}}" class="rounded float-start" alt="..."> </div><input type="checkbox" class="btn-check" id="btn-check-2" checked autocomplete="off">
-            <label class="btn btn-primary" for="btn-check-2">Checked</label>
+        <main>
 
-            <input type="checkbox" class="btn-check" id="btn-check-3" autocomplete="off" disabled>
-            <label class="btn btn-primary" for="btn-check-3">Disabled</label>
+
+            <div class="banner_img"> <img src="{{ asset('images/cesaeimg.png') }}" class="rounded float-start"
+                    alt="..."> </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <input type="checkbox" class="btn-check" id="btn-check-2" checked autocomplete="off">
+                <label class="btn btn-primary d-flex align-items-center" for="btn-check-2">CesaeIn <i class="fa-solid fa-clock icon-large"></i></label>
+                <input type="checkbox" class="btn-check" id="btn-check-3" autocomplete="off" disabled>
+                <label class="btn btn-primary" for="btn-check-3">CesaeOut <i
+                    class="fa-solid fa-clock icon-smal"></i></label>
+            </div>
         </main>
         <!--Container Main end-->
         @yield('content')
