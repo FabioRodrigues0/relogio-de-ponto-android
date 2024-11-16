ca9d4af6d555155f5f6eb712df8086d156b8c3c5
Fábio Rodrigues <56126547+FabioRodrigues0@users.noreply.github.com>
2024-11-16 22:59:46 +0000

    Adiçao das notificações do User

---
+++ MobCesaePulse/app/src/main/java/com/cesaepulse/app/ui/components/NotificationButton/item/NotificationItemViewModel.kt
@@ -0,0 +1,12 @@
+package com.cesaepulse.app.ui.components.NotificationButton.item
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NotificationItemViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
