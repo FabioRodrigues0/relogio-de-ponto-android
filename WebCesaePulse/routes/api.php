27b23d985b896886b4e8dc4f716a5b67c59a756e
Fernando Rodrigues <fernando.r@email.com>
2024-11-22 11:21:47 +0000

    Criação da API da tabela Schedule

--- WebCesaePulse/routes/api.php
+++ WebCesaePulse/routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserApiController;
+use App\Http\Controllers\Api\ScheduleController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::apiResource('/users', UserApiController::class);
+
+
+// Rotas para o ScheduleController
+Route::get('/schedules', [ScheduleController::class, 'index']);  // Listar todos os horários
+Route::get('/schedules/{id}', [ScheduleController::class, 'show']);  // Exibir um horário específico
+Route::put('/schedules/{id}', [ScheduleController::class, 'update']);  // Atualizar um horário
