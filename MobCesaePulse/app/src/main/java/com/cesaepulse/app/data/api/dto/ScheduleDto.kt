78eb2633fbc16a7b116780d9c91ff6f5777b32bc
Fábio Rodrigues <56126547+FabioRodrigues0@users.noreply.github.com>
2024-11-26 18:52:55 +0000

    Criaçao e Implementaçao do repositorio de Schedules

--- MobCesaePulse/app/src/main/java/com/cesaepulse/app/data/api/dto/ScheduleDto.kt
+++ MobCesaePulse/app/src/main/java/com/cesaepulse/app/data/api/dto/ScheduleDto.kt
@@ -1,5 +1,51 @@
 package com.cesaepulse.app.data.api.dto
 
+import com.cesaepulse.app.domain.model.Schedule
+import java.sql.Time
+import java.sql.Timestamp
+
+/**
+ *  Class of ScheduleDto
+ *
+ *  @param id: Int
+ *  @param morning_entry_time: Time
+ *  @param morning_exit_time: Time
+ *  @param afternoon_entry_time: Time
+ *  @param afternoon_exit_time: Time
+ *  @param user_id: Int
+ *  @param created_at: Timestamp
+ *  @param updated_at: Timestamp
+ *
+ */
 data class ScheduleDto(
 	val id: Int,
-)
+	val morning_entry_time: Time,
+	val morning_exit_time: Time,
+	val afternoon_entry_time: Time,
+	val afternoon_exit_time: Time,
+	val user_id: Int,
+	val created_at: Timestamp,
+	val updated_at: Timestamp
+){
+	/**
+	 *  Convert ScheduleDto to Schedule
+	 *
+	 */
+	fun toModel():  Schedule =
+		Schedule(
+			id= id,
+			morning_entry_time = morning_entry_time,
+			morning_exit_time = morning_exit_time,
+			afternoon_entry_time = afternoon_entry_time,
+			afternoon_exit_time = afternoon_exit_time,
+			user_id = user_id,
+			created_at = created_at,
+			updated_at = updated_at
+		)
+}
+
+data class ListScheduleResponse(
+	val data: List<ScheduleDto>
+){
+	fun toModel(): List<Schedule> = data.map { it.toModel() }
+}
---
+++ MobCesaePulse/app/src/main/java/com/cesaepulse/app/data/repository/ScheduleRepository.kt
@@ -0,0 +1,33 @@
+package com.cesaepulse.app.data.repository
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.cesaepulse.app.data.api.CesaePulseApi
+import com.cesaepulse.app.domain.model.Schedule
+import com.cesaepulse.app.domain.repository.IScheduleRepository
+import com.skydoves.sandwich.onError
+import com.skydoves.sandwich.onException
+import com.skydoves.sandwich.onSuccess
+import javax.inject.Inject
+
+class ScheduleRepository @Inject constructor(
+	private val api: CesaePulseApi
+) : IScheduleRepository {
+
+	override suspend fun getSchedulesById(id: Int, month: Int): List<Schedule?> {
+		var schedules: List<Schedule> = emptyList()
+		api.getSchedulesByUserId(id, month)
+			.onSuccess {
+				Log.d("getSchedulesById", "success")
+				schedules = data.toModel()
+			}
+			.onError {
+				Log.e(TAG, "Fail ou getting all schedules from user")
+			}
+			.onException {
+				Log.d("getSchedulesById", "exception - ${this.message}")
+			}
+
+		return schedules
+	}
+}
